8f9981fcae8f469f2b2b114ad58be6c5af2a0d7a
Aditya-Padwal <aditya.padwal3102@gmail.com>
2024-03-08 13:06:48 +0530

    updated

--- node-app/src/11_enums.ts
+++ node-app/src/11_enums.ts
@@ -12,6 +12,10 @@ function doSomething(keyPressed: Direction) {
 };
 
 doSomething(Direction.Up);
+console.log(Direction.Up); // outputs 0
+console.log(Direction.Down); // outputs 1
+console.log(Direction.Left); // outputs 2
+console.log(Direction.Right); // outputs 3
 
 // This makes code slightly cleaner to read out
 // The final value stored at runtime is still a number (0, 1, 2, 3). 
